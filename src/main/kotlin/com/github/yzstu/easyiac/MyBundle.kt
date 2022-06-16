d1b3b2cd87d6bbe5085431787ad55a4263b4c220
GitHub Action <action@github.com>
2022-06-16 14:08:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/yzstu/easyiac/MyBundle.kt
+++ src/main/kotlin/com/github/yzstu/easyiac/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.yzstu.easyiac
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/yzstu/easyiac/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/yzstu/easyiac/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yzstu.easyiac.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yzstu.easyiac.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
